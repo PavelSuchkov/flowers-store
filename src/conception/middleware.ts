8b9e5ea2b5535538ea31f27bdbc9b72f0601b3fa
PavelSuchkov <suchkovp@gmail.com>
2025-04-28 20:01:28 +0300

    made a lot of changes the api works

--- src/conception/middleware.ts
+++ src/conception/middleware.ts
@@ -5,7 +5,7 @@ import { NextFunction, Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(
-      `Request...,', ${req.method}, ${req.path} \n Response... ${res.statusCode}`,
+      `Middleware: ${req.method} ${req.url} ${JSON.stringify(req.body)}`,
     );
     next();
   }
